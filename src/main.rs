318a6e255e9fe9dee60e0666d69d603bde72fc14
Night_Hunter <samuelhuntnz@gmail.com>
2024-03-12 17:38:20 +1300

    add atmosphere

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::{log::LogPlugin, prelude::*};
 use bevy_dev_console::prelude::*;
+use bevy_editor_pls::prelude::*;
 use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 use bevy_gltf_blueprints::{BlueprintsPlugin, GltfFormat};
 use bevy_gltf_save_load::SaveLoadPlugin;
 use bevy_registry_export::ExportRegistryPlugin;
-use bevy_editor_pls::prelude::*;
+use bevy_atmosphere::prelude::{AtmospherePlugin, AtmosphereCamera};
 
 struct BlenderPlugins;
 
@@ -41,6 +42,7 @@ fn main() {
             EditorPlugin::default(),
             DevConsolePlugin,
             NoCameraPlayerPlugin,
+            AtmospherePlugin,
         ))
         .init_state::<MyStates>()
         .add_systems(Startup, (spawn_player, spawn_world, spawn_camera))
@@ -55,8 +57,6 @@ fn spawn_player(mut commands: Commands, assets: Res<AssetServer>) {
     let player = (
         SceneBundle {
             scene: assets.load("models\\library\\person.glb#Scene0"),
-            // scene: assets.load("girl.gltf#Scene0"),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
         },
         Player,
@@ -72,6 +72,7 @@ fn spawn_camera(mut commands: Commands) {
             ..default()
         },
         FlyCam,
+        AtmosphereCamera::default(),
     );
     commands.spawn(camera);
 }
